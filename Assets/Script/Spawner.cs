9c9fe7981c721f7682686d9f2de7a96569807c6d
Visit029MDT <112145096+MDTVisit@users.noreply.github.com>
2023-08-19 20:14:02 +0700

    First-spawn

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject[] Castus;
+
+    [SerializeField] float secondSpawn = 1f;
+
+    [SerializeField] float minTras;
+
+    [SerializeField] float maxTras;
+
     void Start()
     {
-        
+        StartCoroutine(castusspawn());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator castusspawn()
     {
+        
+            while(true)
+            {
+                var wanted = Random.Range(minTras,maxTras);
+                var position = new Vector3(wanted, transform.position.y);
+                GameObject gameObject = Instantiate(Castus[Random.Range(0, Castus.Length)],position,Quaternion.identity);
+                yield return new WaitForSeconds(secondSpawn);
+            }
         
     }
 }
